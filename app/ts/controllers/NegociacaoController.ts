6a6281a02efe652067138caa170d2c68b7a10b68
Ricardo José Rodrigues da Silva <rjrsilva@fazenda.rj.gov.br>
2019-07-29 16:03:22 -0300

    First Commit

---
+++ app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,35 @@
+class NegociacaoController {
+
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
+
+    constructor() {
+
+        this._inputData = <HTMLInputElement>document.querySelector('#data');
+        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+    }
+
+    adiciona(event: Event) {
+
+        event.preventDefault();
+        
+        const negociacao = new Negociacao(
+            new Date(this._inputData.value.replace('/-/g', ',')),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+
+        this._negociacoes.adiciona(negociacao);
+
+        this._negociacoes.paraArray().forEach(negociacao => {
+            console.log(negociacao.data);
+            console.log(negociacao.quantidade);
+            console.log(negociacao.valor);
+        })
+
+        console.log(negociacao);
+    }
+}
