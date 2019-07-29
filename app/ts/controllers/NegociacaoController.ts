1a8455aa3698641a4b92e5fc5c1358f5b1ee32fc
Ricardo José Rodrigues da Silva <rjrsilva@fazenda.rj.gov.br>
2019-07-29 17:24:02 -0300

    Second commit

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -4,12 +4,14 @@ class NegociacaoController {
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
 
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event) {
@@ -24,12 +26,6 @@ class NegociacaoController {
 
         this._negociacoes.adiciona(negociacao);
 
-        this._negociacoes.paraArray().forEach(negociacao => {
-            console.log(negociacao.data);
-            console.log(negociacao.quantidade);
-            console.log(negociacao.valor);
-        })
-
-        console.log(negociacao);
+        this._negociacoesView.update(this._negociacoes);
     }
 }
--- app/ts/models/Negociacoes.ts
+++ app/ts/models/Negociacoes.ts
@@ -2,13 +2,13 @@ class Negociacoes {
 
     private _negociacoes: Negociacao[] = [];
 
-    adiciona(negociacao: Negociacao) {
+    adiciona(negociacao: Negociacao): void {
 
         this._negociacoes.push(negociacao);
 
     }
 
-    paraArray() {
+    paraArray(): Negociacao[] {
         return [].concat(this._negociacoes);
     }
 }
