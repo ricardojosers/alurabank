7fda5d3cc03538720d6e57b92bb975c7a21e72f1
Ricardo José Rodrigues da Silva <rjrsilva@fazenda.rj.gov.br>
2019-07-30 18:01:59 -0300

    Commit Final (parte 1 do curso)

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,5 +1,3 @@
 const controller = new NegociacaoController();
-
-document
-    .querySelector('.form')
-    .addEventListener('submit', controller.adiciona.bind(controller));
+//Usando JQuery
+$('.form').submit(controller.adiciona.bind(controller));
