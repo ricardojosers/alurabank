d481215aacba44443c13bcd95c760f88f2fe264a
Ricardo José Rodrigues da Silva <rjrsilva@fazenda.rj.gov.br>
2019-07-31 15:21:22 -0300

    Segunda parte do curso

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,3 +1,5 @@
+import { NegociacaoController } from "./controllers/NegociacaoController";
+
 const controller = new NegociacaoController();
 //Usando JQuery
 $('.form').submit(controller.adiciona.bind(controller));
